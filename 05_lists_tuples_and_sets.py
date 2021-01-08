ecfa57543945c063365017ed1dd1ab33ec402f25
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-08 14:48:58 +0100

    chore(sections): finish lists, tuples and sets

---
+++ 05_lists_tuples_and_sets.py
@@ -0,0 +1,13 @@
+list = ["Ramsy", "Maan", "Masy"]
+tuple = ("Ramsy", "Maan", "Masy")
+set = {"Bob", "Maan", "Masy"}
+
+list[0] = "Smith"
+
+list.append("Ramsy")
+list.remove("Smith")
+
+set.add("Smith")
+set.add("Smith")
+
+print(list, tuple, set)
