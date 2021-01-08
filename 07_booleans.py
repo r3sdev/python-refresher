b7dca7e942b5eaab96e8fad659b3695c434a5a17
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-08 15:25:06 +0100

    chore(sections): finish booleans

---
+++ 07_booleans.py
@@ -0,0 +1,11 @@
+print(5 == 5)
+print(5 > 5)
+print(10 != 10)
+
+# Python uses the same comparison operators as JS
+
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+print(friends == abroad) # True
+print(friends is abroad) # False, because different 
