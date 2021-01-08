1c3234ef0c25081677eb2c1f465d0c7ad418ba96
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-08 11:02:59 +0100

    chore(sections): finish section 1

---
+++ 01_variables.py
@@ -0,0 +1,26 @@
+# integers, floats
+x = 15
+price = 9.99
+
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result)
+
+# string
+name = "Rolf"
+name = "Bob"
+print(name)
+print(name * 2)
+
+a = 25
+b = a
+
+print(a)
+print(b)
+
+b = 17
+
+print(a)
+print(b)
