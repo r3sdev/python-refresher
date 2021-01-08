b5eeb669cb3e8602b0a03f5762728898b4715296
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-08 15:33:52 +0100

    chore(sections): finish if statements

---
+++ 08_if_statements.py
@@ -0,0 +1,8 @@
+day_of_week = input("What day of the week is it today? ").lower()
+
+if day_of_week == "monday":
+    print("Have a great start of your week")
+elif day_of_week == "friday":
+    print("TGIF =)")
+else:
+    print("Enjoy the rest of your week")
